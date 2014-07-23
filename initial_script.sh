2e2f5a482f1667fdf74adef5947b99adf5c6f3d5
milannic7@gmail.com <milannic7@gmail.com>
2014-07-23 14:32:45 -0400

    diff added

--- initial_script.sh
+++ initial_script.sh
@@ -4,3 +4,5 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ./.vimrc ~/
 sudo apt-get install build-essential ctags cscope libclang-dev clang-3.4 python-dev cmake python-pip
 
+
+#./install.sh --clang-completer
